6e5cdff021d6c0d72f5a96431a90296521d03cae
Maxwell Baugh <max.astroguy@gmail.com>
2020-08-18 14:25:38 -0700

    Update Simple_Bracket_Corrector.py
    Corrected it based on the real answer.  This runs in O(n) time and requires O(n) extra memory for the auxiliary array.

--- Simple_Bracket_Corrector.py
+++ Simple_Bracket_Corrector.py
@@ -11,49 +11,24 @@ For example, if the string is ")a(b((cd)e(f)g)" then return "ab((cd)e(f)g)".
 
 
 def bracket_corrector(s):
-    #First: set up counters & values, we are going to make two passes, once forward then backwards
-    length = len(s)
-    forward_counter = 0
-    reverse_counter = 0
-    values = {'(': 1, ')': -1}
-
-    bad_indices = []
-
-    #Second: make the forward pass, this finds the indices where there are excess closing parentheses
+    #Balance: each closing bracket must match an existing opening bracket
+    #Keep track of brackets on a stack, characters can go about their business
+    
+    stack = []
+    newstring = ['']*len(s)
+    
     for i,char in enumerate(s):
-        #print('i Index: %i, Character: %s' % (i, char))
-        if char in values:
-            forward_counter += values[char]
-            if values[char] < 0 and forward_counter < 0:
-                bad_indices.append(i)
-
-    #print('Bad index list after forward pass')
-    #print(bad_indices)
-
-    #Third: make the backwards pass, this finds the indices where there are excess opening parentheses
-    for j,char in enumerate(s[::-1]):
-        #print('j Index: %i, Character: %s' % (j, char))
-        if char in values:
-            reverse_counter += values[char]
-            if values[char] > 0 and reverse_counter > 0:
-                flipped_index = length - j - 1
-                #print('Flipped Index: %i' % flipped_index)
-                bad_indices.append(flipped_index)
-
-    #print('Final list of bad indices')
-    #print(bad_indices)
-
-    #Fourth: construct new string, excluding the bad indices
-    newstring = ''
-    for k,char in enumerate(s):
-        if k in bad_indices:
-            continue
-        newstring += char
-
-    return newstring
-
-    #This code makes 3 passes over the initial string, doing potentially O(1) operations at each point, therefore
-    #it runs in O(n) time.  The number of bad indices is potentially O(n), therefore it requires O(n) extra space as well.
+        if char == '(':
+            stack.append(i) #Add index of left parenthesis to the stack
+        elif char == ')':
+            idx = stack.pop()        #Retrieve index of most recent left parenthesis
+            newstring[idx] = s[idx]  #Add left parenthesis
+            newstring[i] = char      #Add right parenthesis
+        else:
+            newstring[i] = char  #Add non-parenthesis character
+    
+    #Join new string together & return
+    return ''.join(newstring)
 
 if __name__=='__main__':
     string = str(input())
